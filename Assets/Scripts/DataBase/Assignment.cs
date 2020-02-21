8f86628a5bc0fe5098eceb5e475621ba185716d6
Jiayan Wang <jwang512@asu.edu>
2020-02-20 18:51:32 -0700

    database created

---
+++ Assets/Scripts/DataBase/Assignment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment
+{
+    private int level;
+    private string content;
+    private string answer;
+    private int id;
+
+    public Assignment(int id, int level, string content, string answer)
+    {
+        this.Level = level;
+        this.Content = content;
+        this.Answer = answer;
+        this.id = id;
+    }
+
+    public int Level { get => level; set => level = value; }
+    public string Content { get => content; set => content = value; }
+    public string Answer { get => answer; set => answer = value; }
+}
