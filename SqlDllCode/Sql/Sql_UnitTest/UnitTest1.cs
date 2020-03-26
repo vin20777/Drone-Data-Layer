5a68ca06215955641b120756c1e9df48e3059765
Xinkai Wang <546296781@qq.com>
2020-03-25 20:48:49 -0700

    Add unit test for sql dll

---
+++ SqlDllCode/Sql/Sql_UnitTest/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sql;
+
+namespace Sql_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Select()
+        {
+            SqlEncap sql = new SqlEncap();
+
+            List<string> selectvalue = new List<string>();
+            selectvalue.Add("X");
+            selectvalue.Add("Y");
+            selectvalue.Add("Value");
+
+            string tableName = "Maze";
+
+            Dictionary<string, string> condition = new Dictionary<string, string>();
+            condition.Add("ID", "1");
+            condition.Add("Z", "2");
+
+            Assert.AreEqual("SELECT X,Y,Value FROM Maze WHERE ID = 1 And Z = 2;", sql.Select(selectvalue, tableName, condition));
+        }
+
+        [TestMethod]
+        public void Test_Delete()
+        {
+            SqlEncap sql = new SqlEncap();
+
+            string tableName = "Maze";
+
+            Dictionary<string, string> condition = new Dictionary<string, string>();
+            condition.Add("X", "1");
+            condition.Add("Y", "1");
+            condition.Add("Z", "2");
+
+            Assert.AreEqual("DELETE Maze WHERE X = 1 And Y = 1 And Z = 2;", sql.Delete(tableName, condition));
+        }
+
+        [TestMethod]
+        public void Test_Insert()
+        {
+            SqlEncap sql = new SqlEncap();
+
+            string tableName = "Maze";
+
+            List<string> columnName = new List<string>();
+            columnName.Add("X");
+            columnName.Add("Y");
+            columnName.Add("Z");
+
+            List<string> value = new List<string>();
+            value.Add("1");
+            value.Add("2");
+            value.Add("'maze no.1'");
+
+            Assert.AreEqual("INSERT INTO Maze (X,Y,Z) VALUE (1,2,'maze no.1');", sql.Insert(tableName, columnName, value));
+        }
+
+        [TestMethod]
+        public void Test_Update()
+        {
+            SqlEncap sql = new SqlEncap();
+
+            string tableName = "Maze";
+
+            Dictionary<string, string> setValue = new Dictionary<string, string>();
+            setValue.Add("X", "1");
+            setValue.Add("Y", "1");
+            setValue.Add("Z", "2");
+
+            Dictionary<string, string> condition = new Dictionary<string, string>();
+            condition.Add("ID", "1");
+            condition.Add("MAZE_ID", "9");
+
+            Assert.AreEqual("UPDATE Maze SET X = 1, Y = 1, Z = 2 WHERE ID = 1 And MAZE_ID = 9;", sql.Update(tableName, setValue, condition));
+        }
+    }
+}
