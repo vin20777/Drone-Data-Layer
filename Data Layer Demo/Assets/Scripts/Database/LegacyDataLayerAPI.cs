8661f63d7dd6067c832ce904394181fc27b58982
jywang0 <jwang512@asu.edu>
2020-03-25 20:50:52 -0700

    change api

--- Data Layer Demo/Assets/Scripts/Database/LegacyDataLayerAPI.cs
+++ Data Layer Demo/Assets/Scripts/Database/LegacyDataLayerAPI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Mono.Data.Sqlite;
 using UnityEngine;
@@ -267,4 +267,71 @@ public class LegacyDataLayerAPI
         }
         dbConnection = null;
     }
+
+    public class DBManager
+    {
+        public static SQLiteConnection GetSqlconnection()
+        {
+            SQLiteConnection conn = new SQLiteConnection();
+            try
+            {
+                conn.ConnectionString = "Data Source =Rover.db";
+                conn.Open();
+                conn.Close();
+            }
+            catch
+            {
+
+                throw new Exception("connect failed");
+
+            }
+
+            return conn;
+        }
+
+        //return the line number
+        public static int get(string sql)
+        {
+            SQLiteConnection conn = GetSqlconnection();
+            try
+            {
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+
+        }
+
+        //return the DataTable type of the result. Usually use this.
+        public DataTable getDataTable(string sql)
+        {
+            SQLiteConnection conn = GetSqlconnection();
+            try
+            {
+                conn.Open();
+                SQLiteDataAdapter da = new SQLiteDataAdapter(sql, conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                return ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+
+        }
 }
