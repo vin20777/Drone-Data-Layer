4eb8287697a20d59750f3975a1e1f2e5001a63ed
Xinkai Wang <546296781@qq.com>
2020-03-25 20:13:42 -0700

    Add Update function

--- SqlDllCode/Sql/Sql/SqlEncap.cs
+++ SqlDllCode/Sql/Sql/SqlEncap.cs
@@ -116,5 +116,45 @@ namespace Sql
             return result;
         }
 
+        public string Update(string tableNme, Dictionary<string,string> setValue, Dictionary<string,string> condition)
+        {
+            string result = "";
+
+            result = "UPDATE ";
+            result += tableNme;
+
+            for (int i = 0; i < setValue.Count(); i++)
+            {
+                if (i == 0)
+                {
+                    result += " SET ";
+                }
+                else
+                {
+                    result += ", ";
+                }
+
+                result += setValue.ElementAt(i).Key + " = " + setValue.ElementAt(i).Value;
+            }
+
+            for (int i = 0; i < condition.Count(); i++)
+            {
+                if (i == 0)
+                {
+                    result += " WHERE ";
+                }
+                else
+                {
+                    result += " And ";
+                }
+
+                result += condition.ElementAt(i).Key + " = " + condition.ElementAt(i).Value;
+            }
+
+            result += ";";
+
+            return result;
+        }
+
     }
 }
