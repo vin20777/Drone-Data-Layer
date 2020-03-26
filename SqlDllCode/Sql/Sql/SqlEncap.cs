c71b1be9c7814af14b6580f9333e3602b72b1d67
Xinkai Wang <546296781@qq.com>
2020-03-25 19:05:10 -0700

    Upload code of dll

---
+++ SqlDllCode/Sql/Sql/SqlEncap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql
+{
+    public class SqlEncap
+    {
+        public string Select(List<string>selectValue, string tableNme, Dictionary<string,string>condition)
+        {
+            string result = "";
+
+            result = "SELECT ";
+
+            for(int i = 0; i < selectValue.Count(); i++)
+            {
+                if(i != 0)
+                {
+                    result += ",";
+                }
+
+                result += selectValue[i];
+            }
+
+            result += " FROM " + tableNme;
+
+            for(int i = 0; i < condition.Count(); i++)
+            {
+                if( i == 0)
+                {
+                    result += " WHERE ";
+                }
+                else
+                {
+                    result += " And ";
+                }
+
+                result += condition.ElementAt(i).Key + " = " + condition.ElementAt(i).Value;
+            }
+
+            result += ";";
+
+            return result;
+        }
+    }
+}
